617c557fbcc6b0eb06dd1a00a00888f059a29c54
Elliot Clark <31608028+Elliot-Clark@users.noreply.github.com>
2025-04-21 16:03:23 -0700

    4/21/25 Changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  let testArr = [
+    ["Test", "www.google.com", "www.yahoo.com"],
+    ["Test2", "www.amazon.com", "www.cnn.com"]
+  ]
+    
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>News Aggregator</h1>
+        <span>Your daily dose of news from around the web</span>
+
+        <div id="news">
+
+          {testArr.map((ele, index) => (
+            <div className="newsHeading" key={index}>
+              <h1>{ele[0]}</h1>
+              <p>{ele[1]}</p>
+            </div>
+          ))}
+
+        </div>
       </header>
     </div>
   );
