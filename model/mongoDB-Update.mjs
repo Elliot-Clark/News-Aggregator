01339672efe177fc0eda0174cf4518600289666e
Elliot Clark <31608028+Elliot-Clark@users.noreply.github.com>
2025-05-03 15:54:02 -0700

    Delete feature added for multiple entries on a given day

--- model/mongoDB-Update.mjs
+++ model/mongoDB-Update.mjs
@@ -12,12 +12,14 @@ async function storeData() {
   const collectionName = "headlines";
   const client = new MongoClient(uri);
   try {
+    // Launches the news headlines collection from all the scrapers.
     const summerizedHeadlines = await getAllHeadlines();
     if (summerizedHeadlines.length === 0) {
       console.log("Error fetching any headlines");
       return
     }
 
+    // Inserting data into MongoDB
     await client.connect();
     console.log("Connected successfully to MongoDB", today);
 
@@ -29,6 +31,24 @@ async function storeData() {
       news: summerizedHeadlines 
     });
     console.log("Inserted document with _id:", result.insertedId);
+
+    // If mutliple entries for a day exist, delete the later ones
+    const multipleDateCheck = await collection.find({
+      date: today.toISOString().slice(0, 10),
+    }).toArray();
+    if (multipleDateCheck.length > 1 && multipleDateCheck.length < 8) {
+      multipleDateCheck.pop();
+
+      for (const entry of multipleDateCheck) {
+        try {
+          console.log("Deleting", entry._id);
+          await collection.deleteOne({ _id: entry._id });
+        } catch (e) {
+          console.error("Error deleting entry:", entry._id, e);
+        }
+      }
+    }
+
   } catch (err) {
       console.error("An error occurred:", err);
   } finally {
